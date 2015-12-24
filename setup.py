7af60384a411a2dd2b39832afc13cc86e38ac417
Daniel Baird <JamesDanielBaird@gmail.com>
2015-12-23 22:08:33 -0700

    setup, idk

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 from subprocess import call
 import json
 
-data = {}
-filename = "package_manifest.json"
-try:
-    print "Loading {}".format(filename)
-    with open(filename) as manifest:
-        data = json.load(manifest)
-except IOError:
-    raise Exception("{} missing".format(filename))
 
-add_ppa_call = ["sudo", "add-apt-repository", "--yes"]
-ppas = data["ppa"]
-add_ppa_call.extend(ppas)
-call(add_ppa_call)
-call(["sudo", "apt-get", "update"])
-install_packages_call = ["sudo", "apt-get", "install", "--assume-yes"]
-packages = data["package"]
-install_packages_call.extend(packages)
-print str(install_packages_call)
-call(install_packages_call)
+def package_setup():
+    data = {}
+    filename = "package_manifest.json"
+    try:
+        print "Loading {}".format(filename)
+        with open(filename) as manifest:
+            data = json.load(manifest)
+    except IOError:
+        raise Exception("{} missing".format(filename))
+
+    add_ppa_call = ["sudo", "add-apt-repository", "--yes"]
+    ppas = data["ppa"]
+    add_ppa_call.extend(ppas)
+    call(add_ppa_call)
+    call(["sudo", "apt-get", "update"])
+    install_packages_call = ["sudo", "apt-get", "install", "--assume-yes"]
+    packages = data["package"]
+    install_packages_call.extend(packages)
+    print str(install_packages_call)
+    call(install_packages_call)
+
+
+package_setup()
